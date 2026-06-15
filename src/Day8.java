ec70305723df9a9bc91f5bd2134ded7a7595f88c
gopihar25csc-alt <gopihar25csc@vetias.ac.in>
2026-06-15 09:49:46 +0530

    login

--- src/Day8.java
+++ src/Day8.java
@@ -7,7 +7,7 @@ public class Day8 {
         System.out.print(courseRecord.department());
     }
 }*/
-   enum LANGUGAES {
+   /*enum LANGUGAES {
         TA("Tamil"),
         EN("English"),
         GR("German"),
@@ -21,4 +21,31 @@ public class Day8 {
         System.out.println(LANGUGAES.TA.description);
         System.out.println(LANGUGAES.EN.description);
     }
+}*/
+    sealed interface LoginResult permits LoginSuccess, LoginFailure, LoginLoading {
+    }
+    record LoginSuccess(String time) implements LoginResult {
+    }
+    record LoginFailure(String reason)  implements LoginResult {
+    }
+    record LoginLoading(String expectedTime)  implements LoginResult {
+    }
+    record Random(String reason){
+    }
+    static void handleLogin(LoginResult result) {
+        switch (result) {
+            case LoginSuccess success -> System.out.println("Login Success at : " + success.time());
+            case LoginFailure failure -> System.out.println("Login Failure because of " + failure.reason());
+            case LoginLoading loading -> System.out.println("Login Loading, " + loading.expectedTime());
+        }
+    }
+    static void main() {
+        LoginResult LoginSuccess = new LoginSuccess("2026-06-13:03:03:10");
+        LoginResult LoginFailure = new LoginFailure("Insufficient balance");
+        LoginResult LoginLoading = new LoginLoading("Check after 5 hours");
+        handleLogin(LoginSuccess);
+        handleLogin(LoginFailure);
+        handleLogin(LoginLoading);
+    }
 }
+
