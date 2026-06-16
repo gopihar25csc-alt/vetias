c1967c13886ad2e360f4f160e32ff8d4c187375d
gopiha <gopihar25csc@vetias.ac.in>
2026-06-16 15:14:12 +0530

    remove

--- src/Day10.java
+++ src/Day10.java
@@ -1,21 +1,43 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*import java.util.ArrayList;
 
 public class Day10 {
     static void main() {
         String[] student = {"gopzz", "prasna", "aishu", "nafii"};
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList  ();
         list.add("gopzz");
         list.add("prasna");
         list.add("aishu");
         list.add("nafii");
         for (String s : list) {
-            System.out.print(s);
+            System.out.println(s);
         }
         list.remove("nafii");
         System.out.println();
         for(String s : list) {
-            System.out.print(s);
+            System.out.println(s);
         }
     }
 
+}*/
+public class Day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("hari");
+        queue.add("gopzz");
+        queue.add("madhan");
+        queue.add("dharika");
+        queue.add("shalini");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
+    }
 }
+
+
