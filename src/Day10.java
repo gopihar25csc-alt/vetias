41841fc2e5711a7be3967c514235b4f9004ad999
gopiha <gopihar25csc@vetias.ac.in>
2026-06-17 09:39:29 +0530

    Student

--- src/Day10.java
+++ src/Day10.java
@@ -21,7 +21,7 @@ public class Day10 {
     }
 
 }*/
-public class Day10 {
+/*public class Day10 {
     static void main() {
         LinkedList<String> queue = new LinkedList<String>();
         queue.add("hari");
@@ -35,9 +35,58 @@ public class Day10 {
     }
     static void process(LinkedList<String> queue){
         String removed=queue.removeFirst();
-        System.out.println("Removed:"+removed);
+        System.out.println("Removed:"+removed);  
 
     }
-}
+}*/
+
+        class Student{
+            String name;
+            int regno;
+            int marks;
+            Student(String name ,int regno,int marks){
+                this.name=name;
+                this.regno=regno;
+                this.marks=marks;
+            }
+            @Override
+            public String toString(){
+                return "Name:"+name+
+                        ",regno:"+regno+
+                        ", Marks:"+marks;
+            }}
+        public class Day10{
+            static void main(){
+                LinkedList<Student>students=new LinkedList<>();
+                students.add(new Student("Aishu",101,85));
+                students.add(new Student("ram",102,90));
+                students.add(new Student("pooja",103,87));
+                students.add(new Student("megha",104,78));
+                students.add(new Student("ravi",105,89));
+                System.out.println();
+                for(Student s:students){
+                    System.out.println(s);
+                }
+                students.removeFirst();
+                System.out.println("\nAfter removing first student");
+                for(Student s:students){
+                    System.out.println(s);
+                }
+                String searchvalue="Ravi";
+                for(Student s:students) {
+                    if (s.name.equalsIgnoreCase(searchvalue)) {
+                        s.marks = 95;
+                        break;
+                    }
+                }
+                System.out.println("\nAfter updating Ravi's marks:");
+                for(Student s:students){
+                    System.out.println(s);
+
+                }
+            }
+        }
+
+
 
 
