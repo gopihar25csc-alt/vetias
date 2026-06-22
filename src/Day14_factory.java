33a7771b7466c0ec36c21436e3b3b878cc9a101b
gopiha <gopihar25csc@vetias.ac.in>
2026-06-22 15:00:01 +0530

    sending

---
+++ src/Day14_factory.java
@@ -0,0 +1,74 @@
+/*public class Day14_factory {
+
+    interface Notification {
+        void send(String to, String message);
+    }
+
+    class EmailNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Email: " + message + ", to " + to);
+        }
+    }
+
+    class WhatsappNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Whatsapp: " + message + ", to " + to);
+        }
+    }
+
+    static Notification getNotification(Day14_factory factory, String type) {
+        return switch (type) {
+            case "Email" -> factory.new EmailNotification();
+            case "Whatsapp" -> factory.new WhatsappNotification();
+            default -> throw new IllegalArgumentException(
+                    "Invalid notification type: " + type);
+        };
+    }
+
+    public static void main(String[] args) {
+        Day14_factory factory = new Day14_factory();
+
+        String type = "Whatsapp";
+        getNotification(factory, type)
+                .send("443355", "Hello World");
+    }
+}*/
+public class Day14_factory {
+
+    interface Notification {
+        void send(String to, String message);
+    }
+
+    class CreditNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Credit: " + message + ", to " + to);
+        }
+    }
+
+    class DebitNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Debit: " + message + ", to " + to);
+        }
+    }
+
+    static Notification getNotification(Day14_factory factory, String type) {
+        return switch (type) {
+            case "Credit" -> factory.new CreditNotification();
+            case "Debit" -> factory.new DebitNotification();
+            default -> throw new IllegalArgumentException(
+                    "Invalid notification type: " + type);
+        };
+    }
+
+    public static void main(String[] args) {
+        Day14_factory factory = new Day14_factory();
+
+        String type = "Credit";
+        getNotification(factory, type)
+                .send("557766", "Amount 500 Credited");
+    }
+}
